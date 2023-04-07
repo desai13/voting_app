142586c7061681dc7e408f5b4d09c2c0126638bf
desai13 <42866541+desai13@users.noreply.github.com>
2023-04-07 15:47:51 +0100

    update

--- pages/vote.py
+++ pages/vote.py
@@ -8,7 +8,7 @@ import pydeck as pdk
 
 fauna = fauna_db.Fauna_DB(secret=st.secrets["fauna"])
 names = fauna.get_names()
-options = ['Everything Ravaged, Everything Burned by Wells Tower', 'Annihilation by Jeff Vandermeer', 'In Love by Amy Bloom']
+options = ['Train to Pakistan by Khushwant Singh', 'Middlesex by Jeffrey Eugenides', 'Sea of Tranquility by Emily St. John Mandel']
 number_of_options = len(options)
 
 
@@ -25,6 +25,14 @@ def calc_borda_score(df):
 
 """
 # Vote
+
+Here is a short (AI generated) summary of each of the options:
+1. 'Train to Pakistan' by Khushwant Singh is a historical fiction novel set during the partition of India in 1947. The story follows the lives of the residents of a small village in Punjab as they struggle with the violent religious conflict between Hindus and Muslims that erupted during the partition. The novel offers a nuanced and poignant exploration of the themes of identity, love, sacrifice, and the devastating impact of communal violence.
+
+2. 'Middlesex' by Jeffrey Eugenides is a Pulitzer Prize-winning novel that tells the story of Calliope Stephanides, a Greek-American hermaphrodite who grows up in Detroit. The novel spans three generations of the Stephanides family and explores themes such as identity, sexuality, gender, and the immigrant experience in America.
+
+3. 'Station Eleven' by Emily St. John Mandel is a post-apocalyptic novel that takes place in the aftermath of a devastating flu pandemic that wipes out most of the world's population. The story follows a group of survivors who band together to form a traveling Shakespearean theater troupe as they navigate a world without modern technology or infrastructure. The novel is a haunting meditation on the fragility of human civilization, the enduring power of art, and the bonds that connect us all.
+
 Enter your ranked vote below for for what book you'd like to read, then see the results:
 """
 # audio_file = open('sounds/01. Donda Chant.mp3', 'rb')
